110a751782349273987600cd7ecfc1472eb0ffb0
BLOT Martial <martial.blot@outlook.fr>
2019-04-09 09:51:35 +0200

    Function get cocktail works

---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from  '@angular/core';
+import { Cocktail } from '../app/cocktail.model';
+
+@Injectable({
+    providedIn:  'root'
+})
+
+export  class  CocktailService {
+    public cocktails = [ new Cocktail("BOBORAKA", 24, 'ht' ), new Cocktail("Gin soleil", 18, 'ht' )]
+
+    
+    constructor() { }
+
+    public  getCocktails(){
+        return this.cocktails
+    }
+
+}
